cc839af1e4577da322f18c3bf467767d26173c48
Marius Garbenis <mariusgarbenis2@gmail.com>
2022-04-26 06:48:57 +0300

    logic 220426

--- app/Http/Controllers/ReservationFormController.php
+++ app/Http/Controllers/ReservationFormController.php
@@ -53,9 +53,6 @@ class ReservationFormController extends Controller
                     return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
                 }
             }
-
-
-
             if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $zone->max_people_count)) {
                 return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $zone->max_people_count . ' asmenų.');
             }
@@ -91,187 +88,149 @@ class ReservationFormController extends Controller
             ->where('date_when', $request->input('date_when'))->get();
 
         $reservationFirst = Reservation::where('zone_id', $request->input('zone'))
-                ->where('date_when', $request->input('date_when'))->first();
+            ->where('date_when', $request->input('date_when'))->first();
+
+            $zone = Zone::where('id', $request->input('zone'))->first();
 
 
         if ($request->user()->id == $reservation->user_id) {
-            if ($reservation->zone_id != $request->input('zone')) {
-                $reservation->update([
-                    'zone_id' => $request->zone
-                ]);
-            }
+            if ($request->input()) {
+                if (
+                    ($request->input('zone') != $reservation->zone_id) ||
+                    ($request->input('date_when') != $reservation->date_when) ||
+                    ($request->input('start_time') != $reservation->start_time) ||
+                    ($request->input('end_time') != $reservation->end_time) ||
+                    ($request->input('people_count') != $reservation->people_count)
+                ) {
+                    if ($reservationFirst && ($reservationFirst->id != $reservation->id)) {
+                        if ($request->input('start_time') == $reservationFirst->start_time) {
+                            return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
+                        }
 
-            if ($reservation->people_count != $request->input('people_count')) {
-                if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $reservation->zone->max_people_count)) {
-                    return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $reservation->zone->max_people_count . ' asmenų.');
-                } else {
-                    $reservation->update([
-                        'people_count' => $request->people_count
-                    ]);
-                }
-            }
+                        if (($request->input('start_time') < $reservationFirst->end_time) && ($request->input('start_time') > $reservationFirst->start_time)) {
 
-            if ($reservation->date_when != $request->input('date_when')) {
-                $reservation->update([
-                    'date_when' => $request->date_when
-                ]);
-            }
-            if ($reservation->start_time != $request->input('start_time')) {
-                if ($request->input('start_time') >= $request->input('end_time')){
-                    return back()->with('status', 'Pradžios laikas negali būti vėlesnis arba toks pat kaip pabaigos laiką');
-                }
-                foreach ($otherReservation as $item) {
+                            return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1');
+                        }
 
-                    // if (((substr($item->start_time, 0, 5)) > $request->input('start_time')) && ((substr($item->end_time, 0, 5)) > $request->input('start_time')))
+                        if (($request->input('end_time') < $reservationFirst->end_time) && ($request->input('end_time') > $reservationFirst->start_time)) {
 
-                    if ((substr($item->start_time, 0, 5)) == $request->input('start_time')) {
-                        return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
+                            return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1');
+                        }
+
                     }
-                    if (((substr($item->start_time, 0, 5)) < $request->input('start_time')) && ((substr($item->end_time, 0, 5)) > $request->input('start_time'))) {
-
-                        return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1');
-                    }
-                        $reservation->update([
-                            'start_time' => $request->start_time,
-                        ]);
+                    if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $zone->max_people_count)) {
+                        return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $zone->max_people_count . ' asmenų.');
                     }
 
 
-                }
-            }
+                // foreach ($otherReservation as $item) {
+                    //     if ((substr($item->start_time, 0, 5)) == $request->input('start_time')) {
+                    //         return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
+                    //         break;
+                    //     }
+                    //     if (((substr($item->start_time, 0, 5)) < $request->input('start_time')) && ($request->input('start_time') < (substr($item->end_time, 0, 5)))) {
+                    //         return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1s');
+                    //         break;
+                    //     }
 
-            if ($reservation->end_time != $request->input('end_time')) {
-                if ($request->input('end_time') < $request->input('start_time')){
-                    return back()->with('status', 'Pabaigos laikas negali būti ankstesnis arba toks pat kaip pradžios laikas');
-                }
-                foreach ($otherReservation as $item) {
-
-                    if ((substr($item->end_time, 0, 5)) <= $request->input('end_time')) {
-                        return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-                    }
-                    if (((substr($item->start_time, 0, 5)) == $request->input('end_time')) && ((substr($item->start_time, 0, 5)) > $request->input('end_time'))) {
-                        // dd($item);
-                        return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1');
-                    }
-                        $reservation->update([
-                            'end_time' => $request->end_time,
-                        ]);
-
-                    }
-
-
-
-
-
-            // if($reservationFirst){
-                //     if ($request->input('start_time') == $reservationFirst->start_time) {
-                //         return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-                //     }
-
-                //     if ($request->input('start_time') < $reservationFirst->end_time) {
-                //         return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-                //     }
-                // }
-
-
-
-
-
-                    // $reservation->update([
-                    //     'date_when' => $request->date_when
+                    //     $reservation->update([
+                    //     'zone_id' => $request->zone,
+                    //     'people_count' => $request->people_count,
+                    //     'date_when' => $request->date_when,
+                    //     'start_time' => $request->start_time,
+                    //     'end_time' => $request->end_time
                     // ]);
+                    $reservation->update([
+                        'zone_id' => $request->zone,
+                        'people_count' => $request->people_count,
+                        'date_when' => $request->date_when,
+                        'start_time' => $request->start_time,
+                        'end_time' => $request->end_time
+                    ]);
+
+                }
+            }
 
 
-
-            // if($reservation->zone_id != $request->input('zone')){
+            // if ($reservation->zone_id != $request->input('zone')) {
             //     $reservation->update([
             //         'zone_id' => $request->zone
             //     ]);
+            // }
+
+            // if ($reservation->people_count != $request->input('people_count')) {
+            //     if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $reservation->zone->max_people_count)) {
+            //         return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $reservation->zone->max_people_count . ' asmenų.');
+            //     } else {
+            //         $reservation->update([
+            //             'people_count' => $request->people_count
+            //         ]);
+            //     }
+            // }
+
+            // if ($reservation->date_when != $request->input('date_when')) {
+            //     $reservation->update([
+            //         'date_when' => $request->date_when
+            //     ]);
+            // }
+            // if ($reservation->start_time != $request->input('start_time')) {
+            //     // dd($otherReservation);
+            //     if ($request->input('start_time') >= $request->input('end_time')) {
+            //         return back()->with('status', 'Pradžios laikas negali būti vėlesnis arba toks pat kaip pabaigos laiką');
+            //     }
+            //     foreach ($otherReservation as $item) {
+
+
+
+
+
+            //         if (!(substr($item->start_time, 0, 5)) == $request->input('start_time')) {
+            //             return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
+            //             break;
+            //         }
+            //         if (!((substr($item->start_time, 0, 5)) < $request->input('start_time')) && ($request->input('start_time') < (substr($item->end_time, 0, 5)))) {
+            //             return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1s');
+            //             break;
+            //         }
+
+            //         $reservation->update([
+            //                 'start_time' => $request->start_time,
+            //             ]);
+            //         break;
+            //     }
             // }
 
 
 
 
 
-            // $reservation->update([
-            //             'zone_id' => $request->zone,
-            //             'people_count' => $request->people_count,
-            //             'date_when' => $request->date_when,
-            //             'start_time' => $request->start_time,
-            //             'end_time' => $request->end_time
-            //         ]);
+            // if ($reservation->end_time != $request->input('end_time')) {
+            //     if ($request->input('end_time') < $request->input('start_time')) {
+            //         return back()->with('status', 'Pabaigos laikas negali būti ankstesnis arba toks pat kaip pradžios laikas');
+            //     }
+            //     foreach ($otherReservation as $item) {
+            //         if ($reservation->id != $item->id) {
+            //             if (!(substr($item->end_time, 0, 5)) == $request->input('end_time')) {
+            //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
+            //                 break;
+            //             }
+            //             if (((substr($item->start_time, 0, 5)) < $request->input('end_time')) && ($request->input('end_time') < (substr($item->end_time, 0, 5)))) {
 
-
+            //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota1e');
+            //                 break;
+            //             }
+            //             $reservation->update([
+            //                 'end_time' => $request->end_time,
+            //             ]);
+            //             break;
+            //         }else {
+            //             $reservation->update([
+            //                 'end_time' => $request->end_time,
+            //             ]);
+            //         }
+            //     }
+            // }
         }
-
-        //     if ($request->input('start_time') >= $request->input('end_time')) {
-        //         return back()->with('status', 'Pradžios laikas negali būti vėlesnis arba toks pat kaip pabaigos laiką');
-        //     }
-        //     if ($request->input()) {
-        //         if ($reservation) {
-        //             if ($request->input('start_time') == $reservation->start_time) {
-        //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-        //             }
-
-        //             if ($request->input('start_time') <= $reservation->end_time) {
-        //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-        //             }
-        //         }
-
-
-
-        //         if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $zone->max_people_count)) {
-        //             return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $zone->max_people_count . ' asmenų.');
-        //         }
-        //     }
-
-        //     $request->user()->reservations()->where('id', $reservation->id)->delete();
-
-        //     $request->user()->reservations()->where('id', $reservation->id)->update([
-        //         'zone_id' => $request->zone,
-        //         'people_count' => $request->people_count,
-        //         'date_when' => $request->date_when,
-        //         'start_time' => $request->start_time,
-        //         'end_time' => $request->end_time
-        //     ]);
-        // } else if ($request->user()->level === 'Administratorius' || $request->user()->level === 'Instruktorius') {
-
-        //     if ($request->input('start_time') >= $request->input('end_time')) {
-        //         return back()->with('status', 'Pradžios laikas negali būti vėlesnis arba toks pat kaip pabaigos laiką');
-        //     }
-        //     if ($request->input()) {
-
-        //         $reservation = Reservation::where('zone_id', $request->input('zone'))
-        //             ->where('date_when', $request->input('date_when'))->first();
-        //         $zone = Zone::where('id', $request->input('zone'))->first();
-
-
-        //         if ($reservation) {
-        //             if ($request->input('start_time') == $reservation->start_time) {
-        //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-        //             }
-
-        //             if ($request->input('start_time') < $reservation->end_time) {
-        //                 return back()->with('status', 'Tokiu laiku ši zona jau rezervuota');
-        //             }
-        //         }
-
-
-
-        //         if (($request->input('people_count') <= 0) && ($request->input('people_count') <= $zone->max_people_count)) {
-        //             return back()->with('status', 'Tokiam žmonių kiekiui zona netinkama. Galimas maximalus kiekis ' . $zone->max_people_count . ' asmenų.');
-        //         }
-        //     }
-        //     $reservation->where('id', $reservation->id)->delete();
-        //     $reservation->where('id', $reservation->id)->update([
-        //         'zone_id' => $request->zone,
-        //         'people_count' => $request->people_count,
-        //         'date_when' => $request->date_when,
-        //         'start_time' => $request->start_time,
-        //         'end_time' => $request->end_time
-        //     ]);
-        // }
-
 
         return redirect()->route('reservation');
     }
